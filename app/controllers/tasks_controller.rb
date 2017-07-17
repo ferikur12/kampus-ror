655022f767085c387b5db579019aaa6b7e61f346
ferikur12 <feri.elektro12@gmail.com>
2017-07-18 00:50:53 +0700

    add course and task frontend

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,9 @@
+class TasksController < ApplicationController
+  def show
+  	course = Course.find(params[:course_id])
+  	@tasks = course.tasks
+  	@task = @tasks.find(params[:id])
+
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,22 @@
 Rails.application.routes.draw do
+ # get 'tasks/index'
+
+# get 'tasks/show'
+
+  #get 'courses/index'
+
+  #get 'courses/show'
+
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
-  get 'home/index'
+  #get 'home/index'
 
-  root to: "home#index"
+  root to: "courses#index"
+
+  resources   :courses do
+    resources :tasks, only: [:show]
+  end
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
